9809e331cba216ccd77997f824d070c61456a913
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-10 17:32:42 -0400

    modify moai lua

--- 🗿.lua
+++ 🗿.lua
@@ -1,9 +1,12 @@
-dict = {
+-- * Dictionary
+local dict = {
     ["moai"] = "🗿"
 }
+-- * Safe
 for k, v in pairs(dict) do
     print(k, v)
-    while true do
-        print(v)
-    end
+end
+-- ! Unsafe
+while true do
+    print(dict["moai"])
 end
