a0004ceed5f64266a54c723c9e4aa7ad3b24dc8d
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-13 09:40:41 -0400

    Modify ThemeChanger.js

--- Websites/Website1/ThemeChanger.js
+++ Websites/Website1/ThemeChanger.js
@@ -1,6 +1,6 @@
 const themeToggle = document.getElementById("theme-toggle");
 const themeLink = document.getElementById("theme-link");
-let currentTheme = "Theme1";
+let currentTheme = "Dark";
 
 themeToggle.addEventListener("click", () => {
     currentTheme = currentTheme === "Dark" ? "Design" : "Dark";
