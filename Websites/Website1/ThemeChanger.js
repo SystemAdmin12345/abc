b4db4f892b3eb37ac2b0d922eba74e21e5b1ccbf
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-14 17:55:44 -0400

    Beautify

--- Websites/Website1/ThemeChanger.js
+++ Websites/Website1/ThemeChanger.js
@@ -3,15 +3,14 @@ const themeLink = document.getElementById("theme-link");
 let currentTheme = "Dark";
 
 themeToggle.addEventListener("click", () => {
-    if (currentTheme === "Dark") {
-        currentTheme = "Light";
-    } else if (currentTheme === "Light") {
-        currentTheme = "Design";
-    } else if (currentTheme === "Design") {
-        currentTheme = "Galactic";
-    }
-    else {
-        currentTheme = "Dark";
-    }
-    themeLink.href = `Themes/${currentTheme}.css`;
+  if (currentTheme === "Dark") {
+    currentTheme = "Light";
+  } else if (currentTheme === "Light") {
+    currentTheme = "Design";
+  } else if (currentTheme === "Design") {
+    currentTheme = "Galactic";
+  } else {
+    currentTheme = "Dark";
+  }
+  themeLink.href = `Themes/${currentTheme}.css`;
 });
