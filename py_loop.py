91ec5b750dc49a5fd20a7fa3d04136e10b7fb823
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-23 11:08:06 -0400

    Modify py_loop.py

--- py_loop.py
+++ py_loop.py
@@ -1,32 +1,38 @@
-'''
+"""
 This script contains a basic loop.
-'''
+"""
 
 
 import time
 
+
 # Adjustable Variables
-DELAY = 0.25 # 0.25 by default
+
+DELAY = 0.25  # 0.25 by default
+MIN = 0  # Default: 0
+MAX = 10  # Default: 10
+MAX_CYCLES = 5  # Default: 5
+INCREMENT = 1  # Default: 1
 
 
 def wait(number):
-    '''
+    """
     Shortens the waiting function
-    '''
+    """
     time.sleep(number)
 
 
 # Initialize Variables
 MODE = 0
-NUMBER = 0
+NUMBER = MIN
 CYCLE = 0
 
 print(NUMBER)
-while CYCLE < 10:
+while CYCLE < MAX_CYCLES:
     if MODE == 0:
-        while NUMBER < 10:
-            NUMBER += 1
-            if NUMBER == 10:
+        while NUMBER < MAX:
+            NUMBER += INCREMENT
+            if NUMBER == MAX:
                 break
             print(NUMBER)
             wait(DELAY)
@@ -35,11 +41,10 @@ while CYCLE < 10:
         CYCLE += 1
     wait(DELAY)
     if MODE == 1:
-        while NUMBER > 0:
-            NUMBER -= 1
+        while NUMBER > MIN:
+            NUMBER -= INCREMENT
             print(NUMBER)
             wait(DELAY)
         MODE = 0
         CYCLE += 1
     wait(DELAY)
-    
