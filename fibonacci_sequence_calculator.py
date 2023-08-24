c158bc7886a936392a447b28d933178386ec9e34
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-23 22:19:47 -0400

    Modify fibonacci_sequence_calculator

--- fibonacci_sequence_calculator.py
+++ fibonacci_sequence_calculator.py
@@ -1,14 +1,22 @@
-MAX_CYCLES = 5
+"""
+A calculator for the fibonacci sequence (not 100% accurate)
+"""
+
+
+DATA_1 = 0  # The main number, gets printed.
+DATA_2 = 1  # The secondary number.
+BUFFER = 0  # A temporary buffer to store data before overriding variables.
+CYCLES = 0  # Amount of steps.
+MAX_CYCLES = 5  # Default Variable.
+
+
 try:
     MAX_CYCLES = int(input("How many numbers do you want? "))
 except ValueError:
     print("You put in a non-integer!")
 print(f"Outputting {MAX_CYCLES} number(s).")
 
-DATA_1 = 0
-DATA_2 = 1
-BUFFER = 0
-CYCLES = 0
+
 
 while CYCLES < MAX_CYCLES:
     print(DATA_1)
