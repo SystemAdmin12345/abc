266bcdc9c7bac2eaaf0ec59e871040b3fa7206a6
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-08-24 20:22:35 -0400

    Modify sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -3,7 +3,6 @@
 $localBranch = "master"
 $githubBranch = "master"
 $gitlabBranch = "main"
-
 # Push to the remote branches
 git push origin ${localBranch}:${githubBranch}
 git push gitlab ${localBranch}:${gitlabBranch}
