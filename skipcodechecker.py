0d57b22118d0cd352e76f21bfa19c6155bb05a84
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-09-04 17:36:40 -0400

    clap

--- skipcodechecker.py
+++ skipcodechecker.py
@@ -3,14 +3,24 @@ Hello
 """
 
 INPUT = "No"
+DONE = 0
 
 print("\nWelcome to Skip Code Checker!")
 print("Enter your Skip Code!\n")
-INPUT = input("Skip Code: ")
 
-if INPUT == "I like men":
-    print("\nSo you like men, huh? Skip Code Accepted, giving Gay Badge.....")
-    print("You got a badge! \nBadge: Gay")
-else:
-    print("You put the wrong code!")
+while DONE == 0:
+    INPUT = input("Skip Code: ")
+
+    if INPUT == "gay":
+        DONE = 1
+        print("\nSo you said gay, huh? Skip Code Accepted, giving Gay Badge.....")
+        print("You got a badge! \nBadge: Gay")
+    if INPUT == "Among us":
+        DONE = 1
+        print("AMOGUS!")
+    if INPUT == "Kaching":
+        print("Giving money...")
+        print("Didn't work...\n")
+    else:
+        print(f"You put the wrong code! ({INPUT})")
 print("\n")
